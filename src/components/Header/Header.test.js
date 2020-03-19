5d7e0faf0288a2341da51e0fb02861800abf51bf
Harry Borrelli <hborrelli@outlook.com>
2020-03-18 22:42:36 -0600

    Fix Header component test.

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -4,13 +4,14 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Header', () => {
-  it('displays a logo', () => {
+  it('displays a logo and search component', () => {
     const { getByText } = render(<Header
       news='news'
       searchQuery='chicken'
       setSearchQuery={jest.fn()}
     />);
 
-    expect(getByText('Search Now')).toBeIntheDocument()
+    expect(getByText(/What's/i)).toBeInTheDocument();
+    expect(getByText(/Search Now/i)).toBeInTheDocument();
   });
 });
